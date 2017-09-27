4e9789a6e8aa4e4f66db3074cb4358337db44e1c
William Ning <12452williamning@gmail.com>
2017-09-27 19:17:43 +1300

    Fixed WordCheck and gui is touched a tad

--- src/gui/TestPane.java
+++ src/gui/TestPane.java
@@ -6,12 +6,6 @@ import java.util.Observable;
 import java.util.Observer;
 import java.util.Random;
 
-<<<<<<< HEAD
-
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-=======
->>>>>>> branch
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -39,18 +33,12 @@ public class TestPane extends StackPane implements Observer {
 	private Integer _number;
 	private Label _label;
 
-<<<<<<< HEAD
-	private Button _record;
-	private Button _commitAnswer;
-	private Boolean _playing=false;
-=======
 	private Label _correctness;
 
 	private Button _record;
 	private Button _commitAnswer;
 	private Button _play;
 	private Button _next;
->>>>>>> branch
 
 	private int BUTTON_WIDTH = 160;
 	private int BUTTON_HEIGHT=30;
@@ -81,34 +69,19 @@ public class TestPane extends StackPane implements Observer {
 		_label = new Label();
 		_correctness = new Label();
 
-<<<<<<< HEAD
-		reset();
-
-		_label.setScaleX(5);
-		_label.setScaleY(5);
-
-		_record.setText("Record");
-		_record.setPrefSize(BUTTON_WIDTH, 75d);
-=======
 		_correctness.setTranslateY(-40d);
 		
 
 
 		_record.setText("Record");
 		_record.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
->>>>>>> branch
 
 
 		// Disable for duraion of recording
 		_record.setOnAction(e -> tester.record());
 
-<<<<<<< HEAD
-		_commitAnswer.setText("Commit Answer");
-		_commitAnswer.setPrefSize(BUTTON_WIDTH, 75d);
-=======
 		_commitAnswer.setText("Check Answer");
 		_commitAnswer.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);
->>>>>>> branch
 		_commitAnswer.setDisable(true);
 
 		_commitAnswer.setOnAction(a -> {
@@ -124,64 +97,6 @@ public class TestPane extends StackPane implements Observer {
 			}
 		});
 
-<<<<<<< HEAD
-		Button skip = new Button();
-		skip.setText("Skip");
-		skip.setPrefSize(75d, 30d);		
-		skip.setOnAction(e -> tester.skip());
-
-		Button play = new Button();
-		play.setText("Play");
-		play.setPrefSize(75d, 30d);
-
-		play.setOnAction(e -> {
-			//TODO, if not 
-
-			File playThis=new File("./temp/foo.wav");
-			if (playThis.exists()&&_playing==false) {
-				System.out.println("ok");
-				Media sound=new Media(playThis.toURI().toString());
-				MediaPlayer player=new MediaPlayer(sound);
-				player.play();
-				_playing=true;
-				
-				player.setOnEndOfMedia(new Runnable(){
-					@Override
-					public void run(){
-						_playing=false;
-					}
-				});
-			}
-		});
-
-			Button back = new Button();		
-			back.setText("Exit");
-			back.setPrefSize(BUTTON_WIDTH, 30d);
-
-			back.setOnAction(e -> {
-				// Back button will bring us back to the start menu 
-				_stage.setScene(new Scene(
-						new ListSelectPane(_stage), 
-						FrameConstants.WINDOW_WIDTH, 
-						FrameConstants.WINDOW_HEIGHT
-						));
-				_stage.sizeToScene();
-			});
-
-			smallerBox.getChildren().add(play);
-			smallerBox.getChildren().add(skip);
-
-			// Fix alignment of ButtonBox		
-			buttonBox.getChildren().add(_record);
-			buttonBox.getChildren().add(_commitAnswer);
-			buttonBox.getChildren().add(smallerBox);
-			buttonBox.getChildren().add(back);
-
-			// Put this into a HBox, then put the HBox into the buttonBox		
-			this.getChildren().add(buttonBox);		
-			this.getChildren().add(_label);
-
-=======
 		_next = new Button();
 		_next.setText("Next Question");
 		_next.setPrefSize(BUTTON_WIDTH, BUTTON_HEIGHT);		
@@ -293,26 +208,9 @@ public class TestPane extends StackPane implements Observer {
 			_record.setDisable(false);
 			_commitAnswer.setDisable(false);
 			_play.setDisable(false);
->>>>>>> branch
 		}
-
-		/**
-		 * Reset to a new starting position.
-		 */
-		public void reset() {
-			// Random number generator class to generate numbers.
-			Random randomGenerator = new Random();
-
-			// Depending on the enum, the number attached to this screen will have a different range
-			if (_mode.equals(ListMode.EASY)) {
-				_number = randomGenerator.nextInt(8) + 1;
-			}
-			else if (_mode.equals(ListMode.HARD)) {
-				_number = randomGenerator.nextInt(98) + 1;
-			}
-
-			// Number is in the centre.		
-			_label.setText(_number.toString());
+		else if(recorded == "beginRecord") {
+			_record.setDisable(true);
 			_commitAnswer.setDisable(true);
 			_play.setDisable(true);
 		}
@@ -333,17 +231,6 @@ public class TestPane extends StackPane implements Observer {
 			_next.setDisable(false);
 
 		}
-
-		@Override
-		public void update(Observable arg0, Object recorded) {
-			if (recorded =="endRecord") {
-				_record.setDisable(false);
-				_commitAnswer.setDisable(false);
-			}
-			else if(recorded == "beginRecord") {
-				_record.setDisable(true);
-				_commitAnswer.setDisable(true);
-			}
-		}
-
 	}
+
+}
